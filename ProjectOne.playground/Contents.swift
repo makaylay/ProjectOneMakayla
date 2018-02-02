d1d52a1f2d3c51ae8a7779389397899084cc2758
Makayla <mcsyanos@gmail.com>
2018-02-01 15:29:05 -1000

    Completed Pj 1
    Variables, String Interpolation, Boolean and If Else

---
+++ ProjectOne.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+//Variable Coding Challenge #1
+var firstString = "Aloha, my name is Makayla!"
+let secondString = " Welcome to my Project One"
+var thirdString = firstString + secondString
+print(thirdString)
+
+//Variable Coding Challenge #2
+var celsius = 10
+var fahrenheit = 50
+let degree = "Today is \(celsius) degrees celsius"
+
+//Boolean Variables Coding Challenge #1
+var numberOne = 10
+var numberTwo = 20
+let numberThree = numberOne == numberTwo
+
+//If statement Coding Challenge #1
+var stringOne = "Aloha"
+var stringTwo = "Mahalo"
+let lengthOne = stringOne.characters.count
+let lengthTwo = stringTwo.characters.count
+
+if stringOne>stringTwo
+{
+    print("stringOne is less than than stringTwo")
+}
+else
+{
+    print("stringTwo is greater than stringOne")
+}
